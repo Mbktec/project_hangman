50c7d08a4a811ef13b4c5fa78797ed7aa2679608
YACOUBA ASSOUMANE Moubarak <98525444+Mbktec@users.noreply.github.com>
2022-06-01 01:22:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,129 @@
+const wordE1 = document.getElementById('word');
+const wrongLettersE1 = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = [];
+const wrongLetters = [];
+
+//function generateButtons() {
+   //var buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter => `
+   //<button 
+     //  class="btn btn-lg btn-secondary m-2"
+      // id='` + letter + `'
+     //  onclick="handleGuess('` + letter + `')"
+   //>
+  // ` + letter + `
+   //</button>
+  // `).join('');
+  // document.getElementById('keyword').innerHTML = buttonsHTML;
+//}
+
+//generateButtons()
+
+//show hidden word
+function displayword(){
+   wordE1.innerHTML =`
+   ${selectedWord
+   .split('')
+   .map(
+         letter =>`
+         <span class="letter">
+         ${correctLetters.includes(letter) ? letter: ''}
+         </span>
+       `
+      )
+   .join('')}
+   `;
+   const innerWord = wordE1.innerText.replace(/\n/g, '');
+   
+   if(innerWord === selectedWord){
+      finalMessage.innerText = 'Super! vous avez gagné';
+      playAgainBtn.textContent = 'Rejouier'
+      popup.style.display= 'flex';
+   }
+}
+
+//update the wrong letters
+function uppdateWrongletterE1(){
+   //display wrong letters
+   wrongLettersE1.innerHTML = `
+${wrongLetters.length > 0 ? `<p>Wrong</p>`: ''}
+${wrongLetters.map(letter => `<span>${letter}</span>`)}`;
+
+//display parts
+figureParts.forEach((part, index) => {
+   const errors = wrongLetters.length;
+
+   if(index < errors) {
+      part.style.display = 'block'
+   }
+   else{
+      part.style.display = 'none';
+   }
+});
+//check if lost
+if(wrongLetters.length === figureParts.length){
+   finalMessage.innerText = 'Oups! vous avez perdu la partie';
+   playAgainBtn.textContent = 'Rejouier'
+   popup.style.display = 'flex';
+
+}
+}
+
+//show notification
+function showNotification(){
+   notification.classList.add('show');
+
+   setTimeout(() => {
+      notification.classList.remove('show');
+   }, 2000)
+}
+
+//
+
+window.addEventListener('keydown', e =>{
+   if(e.keyCode>= 65 && e.keyCode <=98){
+      const letter = e.key;
+
+      if(selectedWord.includes(letter)){
+         if(!correctLetters.includes(letter)){
+            correctLetters.push(letter);
+
+            displayword();
+         }
+         else{
+            showNotification();
+         }
+      }else{
+         if(!wrongLetters.includes(letter)){
+           wrongLetters.push(letter); 
+           uppdateWrongletterE1(); 
+         }else{
+            showNotification();
+         }
+      }
+   }
+} );
+
+// star game
+playAgainBtn.addEventListener('click', () =>{
+   correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+selectedWord = words[Math.floor(Math.random() * words.length)];
+displayword();
+
+uppdateWrongletterE1();
+popup.style.display = 'none';
+
+})
+
+displayword();
